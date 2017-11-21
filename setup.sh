2644cfc0767df9f0ecf91a7ded1d5e139b4b6df2
Piotr Kieszczyński <piotr.kieszczynski@nordcloud.com>
2017-11-21 13:34:45 +0100

    Update

--- setup.sh
+++ setup.sh
@@ -2,5 +2,6 @@
 
 git -C ~/.vim/bundle/Vundle.vim  pull || git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
-[ -L $HOME/.vimrc ] && [ -e $HOME/.vimrc ] || ln -s `pwd`/vim/vimrc $HOME/.vimrc
+mkdir -p $HOME/.config/nvim/
+[ -L $HOME/.vimrc ] && [ -e $HOME/.vimrc ] || ln -s `pwd`/vim/vimrc $HOME/.config/nvim/init.vim
 [ -L $HOME/.gitconfig ] && [ -e $HOME/.gitconfig ] || ln -s `pwd`/git/gitconfig $HOME/.gitconfig
