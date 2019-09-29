2941b3ad6d563b5b4979812737bb8cec4e7b6bbe
Piotr Kieszczyński <piotr.kieszczynski@gmail.com>
2019-09-29 20:01:37 +0200

    update slate & gitconfig

--- setup.sh
+++ setup.sh
@@ -5,3 +5,4 @@ git -C ~/.vim/bundle/Vundle.vim  pull || git clone https://github.com/VundleVim/
 mkdir -p $HOME/.config/nvim/
 [ -L $HOME/.vimrc ] && [ -e $HOME/.vimrc ] || ln -s `pwd`/vim/vimrc $HOME/.config/nvim/init.vim
 [ -L $HOME/.gitconfig ] && [ -e $HOME/.gitconfig ] || ln -s `pwd`/git/gitconfig $HOME/.gitconfig
+[ -L $HOME/.slate ] && [ -L $HOME/.slate ] || ln -s `pwd`/slate/slate $HOME/.slate
